26177d8d59c4fcf221d347472c72956afea4cd35
kwardynski <kacper.wardynski@gmail.com>
2022-09-26 21:56:34 -0600

    static text & initial count render done

--- v1_arduino/src/main.cpp
+++ v1_arduino/src/main.cpp
@@ -2,8 +2,10 @@
 #include <TouchScreen.h>
 #include <Adafruit_GFX.h>
 
-#include <Fonts/FreeMonoBold12pt7b.h>
+// #include <Fonts/FreeMonoBold12pt7b.h>
+#include <Fonts/FreeSansBold18pt7b.h>
 #include <Fonts/FreeSans18pt7b.h>
+#include <Fonts/FreeMonoBold18pt7b.h>
 
 // Calibration Values
 const int XP=8,XM=A2,YP=A3,YM=9;
@@ -23,18 +25,28 @@ TouchScreen ts = TouchScreen(XP, YP, XM, YM, 300);
 #define WHITE   0xFFFF
 
 int16_t x1, y1;
-uint16_t w, h;
+uint16_t w, h, text_left;
 uint16_t screen_width = 480;
 uint16_t screen_height = 320;
+uint16_t total_y = 120;
+uint16_t jade_y = 180;
+uint16_t kacper_y = 240;
+
+uint16_t counters_left = 250;
+byte max_treats = 12;
+byte kacper_treats = 0;
+byte jade_treats = 0;
+
 
 void render_title() {
     uint16_t title_buffer = 12;
     String title_text = "Willow's Treat Tracker";
-    tft.setFont(&FreeSans18pt7b);
+    tft.setFont(&FreeSansBold18pt7b);
     tft.getTextBounds(title_text, 0, 0, &x1, &y1, &w, &h);
+    text_left = (screen_width - w - 2*title_buffer)/2;
 
     tft.drawRect(
-        (screen_width - w - 2*title_buffer)/2,
+        text_left,
         title_buffer,
         w + 2*title_buffer,
         h + 2*title_buffer,
@@ -46,7 +58,36 @@ void render_title() {
     tft.print(title_text);
 }
 
-void render_static_text() {}
+
+void render_static_text() {
+    tft.setFont(&FreeSans18pt7b);
+    
+    tft.setCursor(text_left, total_y);
+    tft.print("TOTAL:");
+
+    tft.setCursor(text_left, jade_y);
+    tft.print("Jade:");
+
+    tft.setCursor(text_left, kacper_y);
+    tft.print("Kacper:");
+}
+
+
+void render_treats_count(int used, int max, uint16_t ypos) {
+    char buffer[5];
+    sprintf(buffer, "%02d/%02d", used, max);
+    tft.setFont(&FreeMonoBold18pt7b);
+    tft.setCursor(counters_left, ypos);
+    tft.print(buffer);
+}
+
+
+void render_initial_treats_count() {
+    render_treats_count(0, max_treats, total_y);
+    render_treats_count(kacper_treats, max_treats/2, kacper_y);
+    render_treats_count(jade_treats, max_treats/2, jade_y);
+}
+
 
 void setup(void)
 {
@@ -59,7 +100,7 @@ void setup(void)
 
     render_title();
     render_static_text();
+    render_initial_treats_count();
 }
-
 
 void loop(void) {}
