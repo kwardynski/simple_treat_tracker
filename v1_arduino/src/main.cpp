02b7c6307b607c21ab8ecb99622153ce66053a6d
kwardynski <kacper.wardynski@gmail.com>
2022-10-08 14:57:03 -0600

    main 'restored' - touch not registering bug source found, unknown why yet

--- v1_arduino/src/main.cpp
+++ v1_arduino/src/main.cpp
@@ -42,12 +42,16 @@ uint16_t reset_btn_width;
 uint16_t reset_btn_y = kacper_y+4*button_buffer_y;
 uint16_t reset_btn_x = counters_left;
 
+// Logic Setup
+byte max_treats = 12;
+byte kacper_treats = 0;
+byte jade_treats = 0;
+
 #define BLACK   0x0000
 #define BLUE    0x001F
 #define RED     0xF800
 #define MAGENTA 0xF81F
 #define WHITE   0xFFFF
-
 // #define GREEN   0x07E0
 // #define CYAN    0x07FF
 // #define YELLOW  0xFFE0
@@ -59,6 +63,36 @@ void find_button_dimensions() {
     dec_button_x = counters_left - (button_buffer_x + button_width);
     inc_button_x = counters_left + w + 1.5*button_buffer_x;
     reset_btn_width = w;
+}
+
+void draw_text(uint16_t x, uint16_t y, String text) {
+    tft.setCursor(x, y);
+    tft.print(text);
+}
+
+void render_title() {
+    uint16_t title_buffer = 12;
+    String title_text = "Willow's Treat Tracker";
+    tft.getTextBounds(title_text, 0, 0, &x1, &y1, &w, &h);
+    text_left = (screen_width - w - 2*title_buffer)/2;
+
+    tft.drawRect(
+        text_left,
+        title_buffer,
+        w + 2*title_buffer,
+        h + 2*title_buffer,
+        MAGENTA
+    );
+
+    tft.setCursor((screen_width - w)/2, 2*title_buffer + h);
+    tft.setTextColor(WHITE);
+    tft.print(title_text);
+}
+
+void render_treats_count(int used, int max, uint16_t ypos) {
+    char buffer[5];
+    sprintf(buffer, "%02d/%02d", used, max);
+    draw_text(counters_left, ypos, buffer);
 }
 
 bool get_touch_coordinates(void) {
@@ -114,6 +148,20 @@ void setup(void) {
 
     reset_btn.initButtonUL(&tft, reset_btn_x, reset_btn_y, reset_btn_width, reset_btn_height, WHITE, RED, WHITE, "RESET", 2);
     reset_btn.drawButton(false);
+
+    tft.setFont(&FreeSansBold18pt7b);
+    tft.setTextSize(1);
+    render_title();
+
+    tft.setFont(&FreeSans18pt7b);
+    draw_text(text_left, total_y, "TOTAL:");
+    draw_text(text_left, jade_y, "Jade:");
+    draw_text(text_left, kacper_y, "Kacper");
+
+    tft.setFont(&FreeMonoBold18pt7b);
+    render_treats_count(0, max_treats, total_y);
+    // render_treats_count(0, max_treats/2, jade_y);
+    // render_treats_count(0, max_treats/2, kacper_y);
 
 }
 
